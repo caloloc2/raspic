1ff9302d7e74aea78381e44f2f34d7e2340aa0b8
caloloc2 <calolomino@gmail.com>
2018-08-01 00:25:45 -0500

    actualiza

---
+++ control.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <wiringPi.h>
+ 
+int main (void)
+{
+  printf ("Raspberry Pi blink\n") ;
+ 
+  if (wiringPiSetup () == -1)
+    return 1 ;
+ 
+  /*pinMode (0, OUTPUT) ;         // 
+ 
+  for (;;)
+  {
+    digitalWrite (0, 1) ;       // On
+    delay (500) ;               // mS
+    digitalWrite (0, 0) ;       // Off
+    delay (500) ;
+  }*/
+  return 0 ;
+}
